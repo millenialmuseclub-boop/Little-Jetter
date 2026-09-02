705e7504b3f383dfd8cd25a50ca84863577c82fd
millenialmuseclub-boop <millenialmuseclub@gmail.com>
2026-09-01 18:59:54 -0700

    Promote studio game to main frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { LittleJetterApp } from './LittleJetterApp';
 import { StudioApp } from './studio/StudioApp';
 import { TravelProvider } from './studio/TravelContext';
 import './reset.css';
 
-const studioRoutes = ['/studio', '/style', '/explore', '/journal', '/shop'];
-const isStudio = studioRoutes.some((route) => location.pathname === route || location.pathname.startsWith(`${route}/`));
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isStudio ? <TravelProvider><StudioApp /></TravelProvider> : <LittleJetterApp />}
+    <TravelProvider><StudioApp /></TravelProvider>
   </StrictMode>,
 );
