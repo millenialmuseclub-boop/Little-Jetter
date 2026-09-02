f97b625183a2ad72a57b88a11295cfaf90f7f9c8
millenialmuseclub-boop <millenialmuseclub@gmail.com>
2026-09-01 19:01:49 -0700

    Restore original homepage presentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { LittleJetterApp } from './LittleJetterApp';
 import { StudioApp } from './studio/StudioApp';
 import { TravelProvider } from './studio/TravelContext';
 import './reset.css';
 
+const studioRoutes = ['/studio', '/style', '/explore', '/journal', '/shop'];
+const isStudio = studioRoutes.some((route) => location.pathname === route || location.pathname.startsWith(`${route}/`));
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TravelProvider><StudioApp /></TravelProvider>
+    {isStudio ? <TravelProvider><StudioApp /></TravelProvider> : <LittleJetterApp />}
   </StrictMode>,
 );
